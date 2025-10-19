952369c554fed0f7a251f390b3b3b2d483b6c912
權少 <ljqican@163.com>
2025-10-19 23:32:58 +0800

    添加clangd設置。

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-﻿#include <catch2/../catch2/catch_session.hpp>
+﻿#include <string> // std::to_string
+
+#include <catch2/../catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <fmt/format.h>
 
 TEST_CASE("fmt", "[test]")
 {
+    REQUIRE(std::to_string(42) == "42");
     REQUIRE(fmt::format("The answer is {}.", 42) == "The answer is 42.");
 }
 
